db166c3cab52a80da06277b8c78f090acacdccaa
PIYUJIN <yjyj020201@gmail.com>
2023-06-19 21:51:27 +0900

    add category activity

---
+++ app/src/main/java/com/test/android_memoprogram/CategoryEditActivity.kt
@@ -0,0 +1,11 @@
+package com.test.android_memoprogram
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CategoryEditActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_category_edit)
+    }
+}
